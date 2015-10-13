65a153362a039e32d6ca28d265a2ffe7e2d9ec5d
Tomek Strąk <entrop.tomek@gmail.com>
2015-10-13 18:46:48 +0200

    added gump-autoprefixer, css folder

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('styles', function() {
+
+    gulp.src('css/*.scss')
+        .pipe(sass({
+            errLogToConsole: true
+
+        }))
+
+        .pipe(autoprefixer(
+            {
+                browsers: [
+                    '> 1%',
+                    'last 2 versions',
+                    'firefox >= 4',
+                    'safari 7',
+                    'safari 8',
+                    'IE 9',
+                    'IE 10',
+                    'IE 11'
+                ],
+                cascade: false
+            }
+        ))
+        .pipe(gulp.dest('./css/'))
+});
+
+
+
+//Watch task
+gulp.task('default',function() {
+    //livereload.listen();
+    gulp.watch('css/*.scss',['styles']);
+});
